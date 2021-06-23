05f8553972032d3a9850caa847ccb77459538a11
Deniz Surmeli <52484739+denizsurmeli@users.noreply.github.com>
2021-06-23 20:33:51 +0300

    timewrap changes

--- OLCTest/SpaceGame.cpp
+++ OLCTest/SpaceGame.cpp
@@ -692,7 +692,7 @@ bool SpaceGame::OnUserUpdate(float fElapsedTime) {
 
 				this->totalTime += fElapsedTime;
 
-				std::this_thread::sleep_for(std::chrono::milliseconds(1));
+				std::this_thread::sleep_for(std::chrono::milliseconds(10));
 
 				this->objects.clear();
 
